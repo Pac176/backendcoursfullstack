43eb8f90f66def69a0cbf0938ef252b5dd019214
Godbille Pascal <pascal.godbille@orange.fr>
2020-12-07 11:53:30 +0100

    start

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+//const userRoutes = require('./routes/user');
+
+mongoose.connect('mongodb+srv://Pascal:openclasserooms@cluster0.boxdf.mongodb.net/ClusterO?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); /// tout le monde peut utiliser
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); /// ////entete autorisées
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');/// //methode autorisées
+  next();/// /pour passer au middleware suivant
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+//app.use('/api/auth', userRoutes);
+module.exports = app;
